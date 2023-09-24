2d730338b22d133870241687b06031a1e5826957
Basme Zantout <105554803+bmzantout@users.noreply.github.com>
2023-09-24 13:15:08 +0300

    Update Graph.java

--- Question 1/Graph.java
+++ Question 1/Graph.java
@@ -1,18 +1,13 @@
-package Assignment3_Q1;
-import Assignment1_Q1.Bag;
+import Bag;
 
 public class Graph {
 	
 	//-----------------------------------------------------------------
 	// Title: Question 1
-	// Author: Basme Zantout
-	// ID: 99227947762
-	// Section: 1
-	// Assignment: 3
 	// Description: This is a Graph class, which has the methods that create 
-    //              a graph and that perform basic graph operations based on
-    //              the Graph data structure. This class also depends on 
-    //              the Bag.java class in its implementation
+        //              a graph and that perform basic graph operations based on
+        //              the Graph data structure. This class also depends on 
+        //              the Bag.java class in its implementation
 	//-----------------------------------------------------------------
 	
 	
