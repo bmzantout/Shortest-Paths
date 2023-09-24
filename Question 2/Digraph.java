edeee2732020b7beded52967a2ad4dcd09e44b2c
Basme Zantout <105554803+bmzantout@users.noreply.github.com>
2023-09-24 13:15:51 +0300

    Update Digraph.java

--- Question 2/Digraph.java
+++ Question 2/Digraph.java
@@ -1,18 +1,13 @@
-package Assignment3_Q2;
-import Assignment1_Q1.Bag;
+import Bag;
 
 public class Digraph {
 
 	//-----------------------------------------------------------------
 	// Title: Question 2
-	// Author: Basme Zantout
-	// ID: 99227947762
-	// Section: 1
-	// Assignment: 3
 	// Description: This is a Graph class, which has the methods that create 
-    //              a graph and that perform basic graph operations based on
-    //              the Graph data structure. This class also depends on 
-    //              the Bag.java class in its implementation
+        //              a graph and that perform basic graph operations based on
+        //              the Graph data structure. This class also depends on 
+        //              the Bag.java class in its implementation
 	//-----------------------------------------------------------------
 	
 	
